88627db63c80d52fd3a1fa583dd7b6ea76274e32
SUP3RMASS1VE <Martincull@live.com>
2025-02-23 14:44:08 +0000

    Update install.js

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/C0untFloyd/roop-unleashed app"
+          "git clone https://github.com/SUP3RMASS1VE/RoopUnleashed app"
         ]
       }
     },
