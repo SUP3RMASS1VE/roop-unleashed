5bcfabe7df4db7709ab32720a6b7798a4da3c7da
SUP3RMASS1VE <Martincull@live.com>
2025-02-23 14:51:40 +0000

    Update install.js

--- install.js
+++ install.js
@@ -25,7 +25,7 @@ module.exports = {
         venv: "env",
         path: "app",
         message: [
-          "pip install -r requirements.txt"
+          "uv pip install -r requirements.txt"
         ]
       }
     },
